3988f75c1f79dd6a6aeb157e690adf94120fac90
P4velP <pokr432@gmail.com>
2021-08-20 17:54:39 +0200

    Update Form1.cs

--- Afinni-sifra/Afinni-sifra/Form1.cs
+++ Afinni-sifra/Afinni-sifra/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,7 +12,7 @@ namespace Afinni_sifra
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        public Form1() 
         {
             InitializeComponent();
         }
